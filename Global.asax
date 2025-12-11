7330768253a0299c790b813bf925406e398f346a
IamMattHenry <matthenry1005200@gmail.com>
2025-12-11 12:05:31 +0800

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BUENAVENTURA_MATTHENRY_FinalTermActivity_4_5.MvcApplication" Language="C#" %>
---
+++ Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BUENAVENTURA_MATTHENRY_FinalTermActivity_4_5.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+
+    }
+}
